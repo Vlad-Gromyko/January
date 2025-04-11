1eb2e8b9fdec1fe6d1591995a2a27cc88e0d3f77
vgrom <spacethematrix@gmail.com>
2025-04-11 18:32:17 +0700

    уа

--- application/core/services/nodes/nodes_types/holograms/add.py
+++ application/core/services/nodes/nodes_types/holograms/add.py
@@ -12,29 +12,26 @@ class Node(INode):
 
         self.special_id = special_id
 
-        self.add_enter_socket('Max', self.palette['NUM'])
-        self.add_enter_socket('Min', self.palette['NUM'])
+        self.add_enter_socket('A', self.palette['HOLOGRAM'])
+        self.add_enter_socket('B', self.palette['HOLOGRAM'])
 
-        self.add_output_socket('int', self.palette['NUM'])
-        self.add_output_socket('float', self.palette['NUM'])
+        self.add_output_socket('', self.palette['HOLOGRAM'])
+
 
     def execute(self):
         arguments = self.get_func_inputs()
 
-        num1 = arguments['Min']
-        num2 = arguments['Max']
+        a = arguments['A']
+        b = arguments['B']
 
-        value = np.random.uniform(num1, num2)
-
-        self.output_sockets['int'].set_value(int(value))
-        self.output_sockets['float'].set_value(value)
+        self.output_sockets[''].set_value(a+b)
 
         if 'go' in self.output_sockets.keys():
             self.output_sockets['go'].set_value(True)
 
     @staticmethod
     def create_info():
-        return Node, 'Рандом', 'math'
+        return Node, 'Сложение', 'hologram'
 
     @staticmethod
     def possible_to_create():
